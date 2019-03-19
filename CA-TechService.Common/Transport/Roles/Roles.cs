b9609c4589be8937a05d0ad14c4b589479588170
chethangu <mailtochethankumar@gmail.com>
2019-03-18 19:02:17 -0700

    New classes added

---
+++ CA-TechService.Common/Transport/Roles/Roles.cs
@@ -0,0 +1,13 @@
+﻿#region Imports
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+namespace CA_TechService.Common.Transport.Roles
+{
+    public class Roles
+    {
+    }
+}
---
+++ CA-TechService.Data/Query/ClientMaster/ClientMasterQueries.cs
@@ -0,0 +1,14 @@
+﻿#region Imports
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+namespace CA_TechService.Data.Query.ClientMaster
+{
+  public class ClientMasterQueries
+    {
+    }
+}
+ 

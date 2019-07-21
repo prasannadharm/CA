49c2570899de6c5ddb35dca7bc5ca7197b210c58
chethangu <mailtochethankumar@gmail.com>
2019-07-20 20:26:53 -0700

    Task Stage Master Changes

--- CA-TechService.Data/Query/ClientMaster/ClientMasterQueries.cs
+++ CA-TechService.Data/Query/ClientMaster/ClientMasterQueries.cs
@@ -9,6 +9,15 @@ namespace CA_TechService.Data.Query.ClientMaster
 {
   public class ClientMasterQueries
     {
+        public static string GetClientCategoryList { get { return "USP_GetClientCategoryList"; } }
+
+        public static string InsertClientCategoryMaster { get { return "USP_InsertClientCategoryMaster"; } }
+
+        public static string UpdateClientCategoryMaster { get { return "USP_UpdateClientCategoryMaster"; } }
+
+        public static string DeleteClientCategoryMaster { get { return "USP_DeleteClientCategoryMaster"; } }
+
+        public static string GetClientCategoryDetailsbyID { get { return "USP_GetClientCategoryDetailsbyID"; } }
     }
 }
  
---
+++ CA-TechServices/Pages/TaskMaster/TaskStageMaster.aspx.cs
@@ -0,0 +1,98 @@
+﻿#region Imports
+using System;
+using System.Collections.Generic;
+using CA_TechService.Common.Generic;
+using CA_TechService.Common.Transport.ClientMaster;
+using CA_TechService.Data.DataSource.ClientMaster;
+using System.Web.Services;
+using CA_TechService.Common.Transport.TaskMaster;
+using CA_TechService.Data.DataSource.TaskMaster;
+#endregion
+namespace CA_TechServices.Pages.TaskMaster
+{
+    public partial class TaskStageMaster : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        [WebMethod]
+        public static TaskStageMasterEntity[] GetData() //Show the details of the data after insert in HTML Table
+        {
+            var details = new List<TaskStageMasterEntity>();
+            try
+            {
+                details = new TaskStageMasterDAO().GetTaskStageMasterList();
+            }
+            catch (Exception ex)
+            {
+                // details.Add(new DbStatusEntity(ex.Message));
+            }
+            return details.ToArray();
+        }
+
+        [WebMethod]
+        public static TaskStageMasterEntity[] EditData(int id)
+        {
+            var details = new List<TaskStageMasterEntity>();
+            try
+            {
+                details = new TaskStageMasterDAO().EditTaskStageMaster(id);
+            }
+            catch (Exception ex)
+            {
+                //details.Add(new DbStatusEntity(ex.Message));
+            }
+            return details.ToArray();
+        }
+
+        [WebMethod]
+        public static DbStatusEntity[] UpdateData(TaskStageMasterEntity obj, int id) //Update data in database  
+        {
+            var details = new List<DbStatusEntity>();
+            try
+            {
+                details.Add(new TaskStageMasterDAO().UpdateTaskStageMaster(obj, id));
+            }
+            catch (Exception ex)
+            {
+                details.Clear();
+                details.Add(new DbStatusEntity(ex.Message));
+            }
+            return details.ToArray();
+
+        }
+
+        [WebMethod]
+        public static DbStatusEntity[] InsertData(TaskStageMasterEntity obj)
+        {
+            var details = new List<DbStatusEntity>();
+            try
+            {
+                details.Add(new TaskStageMasterDAO().InsertTaskStageMaster(obj));
+            }
+            catch (Exception ex)
+            {
+                details.Clear();
+                details.Add(new DbStatusEntity(ex.Message));
+            }
+            return details.ToArray();
+        }
+
+        [WebMethod]
+        public static DbStatusEntity[] DeleteData(int id)
+        {
+            var details = new List<DbStatusEntity>();
+            try
+            {
+                details.Add(new TaskStageMasterDAO().DeleteTaskStageMaster(id));
+            }
+            catch (Exception ex)
+            {
+                details.Clear();
+                details.Add(new DbStatusEntity(ex.Message));
+            }
+            return details.ToArray();
+        }
+    }
+}
